7bad1b7480216dbfab024d700a74b606554fe88b
loki283@free.fr <loki283@free.fr>
2017-11-18 00:15:07 +0100

    Ajout de helpers pour le parseur

--- Expression.hs
+++ Expression.hs
@@ -4,7 +4,14 @@ module Expression where
 1. Doit on mettre dans des modules différents ?
 2. Est ce qu'on doit changer la grammaire ? pour éviter d'avoir des parenthèse obligatoire pour le parseur
 3. Doit on mettre les déclarations dans un fichier à part ?
+4. getOperator vs map
+5. Op2 & Op1 en tant que class ?
 -}
+
+-- data Op1 = Op1 {
+    -- name :: Char
+    -- operator :: (a -> a)
+-- }
 ------------------------ Expr ---------------------------
 
 data Expr = Const Float
@@ -16,15 +23,25 @@ data Expr = Const Float
 data Op1 = Minus deriving Show
 data Op2 = Add | Mult | Pow deriving Show
 
-getOperator1 :: Num a => Op1 -> (a -> a)
-getOperator1 op = case op of
+--Helpers
+getOperatorFromOp1 :: Num a => Op1 -> (a -> a)
+getOperatorFromOp1 op = case op of
     Minus -> negate
     
-getOperator2 :: Floating a => Op2 -> (a -> a -> a)
-getOperator2 op = case op of
+getOperatorFromOp2 :: Floating a => Op2 -> (a -> a -> a)
+getOperatorFromOp2 op = case op of
     Add -> (+)
     Mult -> (*)
     Pow -> (**)
+    
+getUniOp op = case op of
+    minusChar -> Minus
+
+getBinOp :: Char -> Op2
+getBinOp op = case op of
+    '+' -> Add
+    '*' -> Mult
+    '^' -> Pow
 
 ------------------------ Store --------------------------------
 
@@ -49,13 +66,13 @@ eval s expr = case expr of
     Const c -> Just c
     Variable v -> findVar s v
     Uni o e -> let r = eval s e in
-               let op = getOperator1 o in
+               let op = getOperatorFromOp1 o in
                case r of
                     Nothing -> Nothing
                     Just v -> Just (op v)
     Bin o e1 e2 -> let r1 = eval s e1 in
                    let r2 = eval s e2 in
-                   let op = getOperator2 o in
+                   let op = getOperatorFromOp2 o in
                    case (r1, r2) of
                        (Just v1, Just v2) -> Just (op v1 v2)
                        (_, _) -> Nothing
