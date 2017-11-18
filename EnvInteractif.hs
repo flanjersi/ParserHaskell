a4976dbecc856a98c8e75fef6740d6a9460a3062
loki283@free.fr <loki283@free.fr>
2017-11-18 02:19:28 +0100

    Gestion de la boucle principale et de l'evaluation d'une expression

---
+++ EnvInteractif.hs
@@ -0,0 +1,23 @@
+module EnvInteractif where
+
+import Parse
+import Expression
+import Data.Maybe
+
+---------------------- Commande -------------------------------
+store = initStore
+
+isCommand xs =
+    if head xs == ':' then True else False
+
+processExpression line =
+    let parsedLine = parseExpression line in
+    eval store (fromJust parsedLine)
+
+main = do
+    putStr "> "
+    line <- getLine
+    if isCommand line == False
+        then putStrLn (show (processExpression line))
+        else putStrLn "0"
+    main
